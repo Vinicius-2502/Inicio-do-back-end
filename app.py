1b1f71e5668f6c2e30fd0b6cce02abd01ce15e7d
Vinicius-2502 <vinicius.nascimento.250209@gmail.com>
2025-05-28 16:41:08 -0300

    Routes to get product details and update.

--- app.py
+++ app.py
@@ -32,6 +32,36 @@ def delete_product(product_id):
         return jsonify({"message": "Product deleted successfully"})
    return jsonify({"message": "Product not found"}), 404
 
+@app.route('/api/products/<int:product_id>', methods=["GET"])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "description": product.description
+        })
+    return jsonify({"message": "Product not found"}), 404
+
+@app.route('/api/products/update/<int:product_id>', methods=["PUT"])
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+            return jsonify({"message": "Product not found"}), 404
+
+    data = request.json
+    if "name" in data:
+        product.name = data["name"]
+
+    if "price" in data:
+        product.price = data["price"]
+
+    if "description" in data:
+        product.description = data["description"]
+
+    db.session.commit()
+    return jsonify({"message": "Product updated successfully"})
 
 #definir uma rota raiz(pagina inicial) e a função que será executada ao requisitar
 @app.route('/')
